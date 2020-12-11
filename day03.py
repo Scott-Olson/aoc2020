20ee4f65e9c5b233b8c4c4e0db70025f41085d38
scott-olson <scotteolson3@gmail.com>
2020-12-11 15:39:37 -0800

    Day 3 p2 complete

--- day03.py
+++ day03.py
@@ -1,4 +1,5 @@
 input = open('in03.txt', 'r')
+import math
 
 loc = input.readlines()
 clean_loc = []
@@ -12,24 +13,42 @@ print(clean_loc[0])
 print(f"len: {line_l} 0: {clean_loc[0][0]} len: {clean_loc[0][line_l - 1]}")
 cur = clean_loc[0]
 
-cur_x = 0
-cur_y = 0
-trees = 0
+# cur_x = 0
+# cur_y = 0
+# trees = 0
 
-while cur_y < l:
-	# print(f"x:{cur_x}, y:{cur_y}, l:{line_l}")
-	if clean_loc[cur_y][cur_x] == '#':
-		trees += 1
-	cur_x += 3
-	# print(f"after add x:{cur_x}, y:{cur_y}, l:{line_l}")
-	if line_l <= cur_x:
-		# print("Before mod: ", cur_x)
-		cur_x = cur_x % line_l
-		# print("After mod: ", cur_x)
-	cur_y += 1
-	# print(trees)
+# while cur_y < l:
+# 	if clean_loc[cur_y][cur_x] == '#':
+# 		trees += 1
+# 	cur_x += 3
+# 	if line_l <= cur_x:
+# 		cur_x = cur_x % line_l
+# 	cur_y += 1
 
-print("# of trees seen: ", trees)
+# print("# of trees seen: ", trees)
 # 207, correct
 
 # part 2
+
+def trav(step_x, step_y):
+	cur_x, cur_y, trees = 0, 0, 0
+	while cur_y < l:
+		if clean_loc[cur_y][cur_x] == '#':
+			trees += 1
+		cur_x += step_x
+		if line_l <= cur_x:
+			cur_x = cur_x % line_l
+		cur_y += step_y
+	return trees
+
+res = [trav(1, 1),trav(3, 1), trav(5, 1), trav(7, 1), trav(1, 2)]
+print("1, 1: ", res[0])
+print("3, 1: ", res[1])
+print("5, 1: ", res[2])
+print("7, 1: ", res[3])
+print("1, 2: ", res[4])
+print(math.prod(res))
+
+# 2655892800 correct answer
+
+
